10e7549fb4a3526693caa4874506c897d4cd2c8d
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-13 18:28:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  //const form = document.getElementById("create-task-form");
- // form.addEventListener("submit", addToList);
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", addToDo);
 });
 
-//const userInput = document.getElementById("new-task-description");
-//const tasks = document.getElementById("tasks")
+const userInput = document.getElementById("new-task-description");
+const tasks = document.getElementById("tasks")
 
-//const addToList = event => { 
-//  event.preventDefault();
-//  const li = document.createElement("li"); 
-//  tasks.appendChild(li).innerText = userInput.value; 
-//  event.target.reset();
-//}
+const addToDo = event => { 
+  event.preventDefault();
+  const li = document.createElement("li"); 
+  tasks.appendChild(li).innerText = userInput.value; 
+  event.target.reset();
+}
