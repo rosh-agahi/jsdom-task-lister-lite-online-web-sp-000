567b55c7ff10712cf40fda1d3ab6128478b3996a
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-13 18:35:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,3 +13,15 @@ const addToDo = event => {
   tasks.appendChild(li).innerText = userInput.value; 
   event.target.reset();
 }
+
+//alphaSort
+
+$(function(){
+    function alphaSort(a, b){
+        if(a.innerText > b.innerText) return 1;
+        if(a.innerText < b.innerText) return -1;
+        return 0;
+    }
+    var ul = $('ul');
+    ul.html(ul.find('li').sort(alphaSort));
+})
